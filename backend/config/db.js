45d82c0b02deed4705b6cd178d1c088837c0f3de
mrad-beep <shrotriyamradul8@gmail.com>
2026-07-03 11:02:48 +0530

    Second commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+{/*const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
@@ -6,6 +6,26 @@ const connectDB = async () => {
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`MongoDB connection error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;*/}
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    console.log("Connecting to MongoDB...");
+    console.log(process.env.MONGO_URI);
+
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 5000,
+    });
+
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("FULL ERROR:");
+    console.error(error);
     process.exit(1);
   }
 };
